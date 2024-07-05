5e52ef85db42c5d27696c47045c0ebdb5e1d1342
FarjanaAkhi <akhimim693@gmail.com>
2024-07-05 17:34:06 +0600

    311 project

---
+++ pet.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> pet agency </title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+  </head>
+
+<body>
+  
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+ <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Our Pets</a>
+      </li>
+    <ul class="navbar-nav ml-auto">
+      
+   
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+</nav> 
+
+
+<section>
+  <div class="py-5">
+    <h3 class="text-center"></h3>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+
+      
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/c1.jpg" class="img-fluid">
+            <h5 class="card-title">Leo</h5>
+            <p class="card-text">Calico breed cat</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/c2.jpg" class="img-fluid">
+            <h5 class="card-title">Fiyona</h5>
+            <p class="card-text">Ginger color mixed breed cat</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/d.jpg" class="img-fluid">
+            <h5 class="card-title">Piku</h5>
+            <p class="card-text">Japanese Spitz</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/d2.jpg" class="img-fluid">
+            <h5 class="card-title">Maximus</h5>
+            <p class="card-text">Japanese Spitz</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+     
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/d.jpg" class="img-fluid">
+            <h5 class="card-title">Max</h5>
+            <p class="card-text">British Shorthair</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/c6.jpg" class="img-fluid">
+            <h5 class="card-title">zoe</h5>
+            <p class="card-text">Persian breed cat</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/c3.jpg" class="img-fluid">
+            <h5 class="card-title">Milo</h5>
+            <p class="card-text">Persian breed cat</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img src="image/c3.jpg" class="img-fluid">
+            <h5 class="card-title">Luna</h5>
+            <p class="card-text">Persian breed cat</p>
+            <a href="register.php" class="btn btn-primary">Click here to adopt</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+<div >
+<footer>
+    <p class="p-3 bg-dark text-white text-center"> fk@gamil.com</p>
+</footer>
+</div> 
+</body>
+</html>
